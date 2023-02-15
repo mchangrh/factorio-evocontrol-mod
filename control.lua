a2e7fa08d0ed3c577db8e8dd6b49b6017f92f14a
Michael C <michael@mchang.name>
2023-02-15 00:35:11 -0500

    simplify command output

--- control.lua
+++ control.lua
@@ -1,3 +1,4 @@
+-- Set Evolution Function
 function set_evolution()
   overrides = settings.startup
   map_settings = game.map_settings["enemy_evolution"]
@@ -5,22 +6,22 @@ function set_evolution()
   map_settings.pollution_factor = map_settings.pollution_factor * overrides["evocontrol-pollution-multiplier"].value
   map_settings.destroy_factor = map_settings.destroy_factor * overrides["evocontrol-destroy-multiplier"].value
 end
+-- Add Commands
 commands.add_command("evoctrl_multiplier", "get evolution multipliers", function(command)
   overrides = settings.startup
-  output = {}
-  output["Time"] = "Time: " .. overrides["evocontrol-time-multiplier"].value .. "x"
-  output["Pollution"] = "Pollution: " .. overrides["evocontrol-pollution-multiplier"].value .. "x"
-  output["Spawners"] = "Spawners: " .. overrides["evocontrol-destroy-multiplier"].value .. "x"
-  game.print("Evolution factors: (" .. output.Time .. ") (" .. output.Pollution .. ") (" .. output.Spawners .. ")")
+  time = "(Time: " .. overrides["evocontrol-time-multiplier"].value .. "x) "
+  pollution = "(Pollution: " .. overrides["evocontrol-pollution-multiplier"].value .. "x) "
+  spawners = "(Spawners: " .. overrides["evocontrol-destroy-multiplier"].value .. "x)"
+  game.print("Evolution factors: " .. time .. pollution .. spawners)
 end)
 commands.add_command("evoctrl_values", "get evolution values", function(command)
   map_settings = game.map_settings["enemy_evolution"]
-  output = {}
-  output["Time"] = "Time: " .. map_settings.time_factor
-  output["Pollution"] = "Pollution: " .. map_settings.pollution_factor
-  output["Spawners"] = "Spawners: " .. map_settings.destroy_factor
-  game.print("Evolution values: (" .. output.Time .. ") (" .. output.Pollution .. ") (" .. output.Spawners .. ")")
+  time = "(Time: " .. map_settings.time_factor .. ") "
+  pollution = "(Pollution: " .. map_settings.pollution_factor .. ") "
+  spawners = "(Spawners: " .. map_settings.destroy_factor .. ")"
+  game.print("Evolution values: " .. time .. pollution .. spawners)
 end)
+-- Run on init
 script.on_init(function()
   set_evolution()
 end)
--- settings.lua
+++ settings.lua
@@ -3,21 +3,21 @@ data:extend({
     type = "double-setting",
     name = "evocontrol-time-multiplier",
     setting_type = "startup",
-    default_value = 1.0,
+    default_value = 1,
     minimum_value = 0,
   },
   {
     type = "double-setting",
     name = "evocontrol-destroy-multiplier",
     setting_type = "startup",
-    default_value = 1.0,
+    default_value = 1,
     minimum_value = 0,
   },
   {
     type = "double-setting",
     name = "evocontrol-pollution-multiplier",
     setting_type = "startup",
-    default_value = 1.0,
+    default_value = 1,
     minimum_value = 0,
   }
 })
